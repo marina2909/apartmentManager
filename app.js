3a97d160f45500026e29adfef519b6933136a09d
Marina Sovic <sovic.marina@gmail.com>
2015-10-07 00:46:12 -0700

    Use apartments and guests in main file

--- app.js
+++ app.js
@@ -7,10 +7,12 @@ var mongoose = require('mongoose');
 mongoose.connect("mongodb://localhost:27017/manageDb");
 require('./models/models.js');
 
+var guests = require('./routes/guests');
 var apartments = require('./routes/apartments');
 app.use(express.static('public'));
 
-app.use('/apartments', apartments); 
+app.use('/guests', guests);
+app.use('/apartments', apartments);
 
 
 app.listen(3000, function(){

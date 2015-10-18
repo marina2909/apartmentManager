e61ad4801edcb074e08f0e3f3b01e90a1d253012
Marina Sovic <sovic.marina@gmail.com>
2015-10-18 11:21:34 -0700

    Fix apartment directive

--- public/javascript/directives/am-apartment.js
+++ public/javascript/directives/am-apartment.js
@@ -1,19 +1,20 @@
 angular.module("apartmentManagement")
- .directive("amApartment", function($http, Apartment){
+ .directive("amApartment", function($http, Apartment, $location){
    return {
      restrict: 'E',
      templateUrl : "templates/directives/am-apartment.html",
      scope:{
        apartment : "="
      },
-     controller : function($scope){
+     link : function($scope){
        $scope.removeApartment = function(event, name){
          var that = event.target;
-         if (!confirm('Are you sure you want to delete this record')){
+         if (!confirm('Are you sure you want to delete this record?')){
            return false;
          }
          Apartment.delete(name).then(function success(res){
-           $(that).closest('am-apartment').parent().remove();
+          // $(that).closest('am-apartment').parent().remove();
+            $location.path('/apartments');
          }, function error(err){
            // TODO error handling
          });

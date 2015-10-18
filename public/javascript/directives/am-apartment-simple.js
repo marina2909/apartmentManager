efabc6075a000ae1fdf7256340cdc0d330961492
Marina Sovic <sovic.marina@gmail.com>
2015-10-18 11:04:34 -0700

    Add apartment simple directive for apartment list

---
+++ public/javascript/directives/am-apartment-simple.js
@@ -0,0 +1,10 @@
+angular.module("apartmentManagement")
+ .directive("amApartmentSimple", function($http, Apartment){
+   return {
+     restrict: 'E',
+     templateUrl : "templates/directives/am-apartment-simple.html",
+     scope:{
+       apartment : "="
+     }
+   };
+})

c4a5352cb47740a8d66f17d3d7ddf94ff8dec3b3
Marina Sovic <sovic.marina@gmail.com>
2015-10-19 00:57:50 -0700

    Add search bar and apartment-form directives

---
+++ public/javascript/directives/am-apartment-form.js
@@ -0,0 +1,16 @@
+angular.module("apartmentManagement")
+.directive("amApartmentForm", function(){
+  return {
+    restrict : 'E',
+    templateUrl : 'templates/directives/am-apartment-form.html',
+    scope : {
+      apartment : '=',
+      submit : '&submit'
+    },
+    link : function(scope){
+      scope.submitApartment = function(apartment){
+        scope.submit(apartment);
+      }
+    }
+  }
+});
